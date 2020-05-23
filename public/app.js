2a9000cc0888d959616163d413c80d669a2f66d1
Patrick <patrick204nqh@gmail.com>
2020-05-23 12:41:25 +0700

    interfaces

--- public/app.js
+++ public/app.js
@@ -1,12 +1,25 @@
+const me = {
+    name: 'patrick',
+    age: 20,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spend', amount);
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log('hello', person.name);
+};
+greetPerson(me);
+console.log(me);
 import { Invoice } from './classes/Invoice.js';
 const invOne = new Invoice('patrick', 'work on the patrick website', 250);
 const invTwo = new Invoice('robert', 'work on the robert website', 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
-});
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+// interfaces
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: 'patrick',
+  age: 20,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spend', amount);
+    return amount;
+  },
+};
+
+const greetPerson = (person: IsPerson) => {
+  console.log('hello', person.name);
+};
+
+greetPerson(me);
+
+console.log(me);
+
 import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('patrick', 'work on the patrick website', 250);
@@ -6,10 +34,6 @@ const invTwo = new Invoice('robert', 'work on the robert website', 300);
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-
-invoices.forEach((inv) => {
-  console.log(inv.client, inv.amount, inv.format());
-});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
