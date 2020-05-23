50e1f0ee90f2365c24ef40667b9c439f7080ff4f
Patrick <patrick204nqh@gmail.com>
2020-05-23 12:32:03 +0700

    modules

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
-// classes
-class Invoice {
-  // readonly client: string;
-  // private details: string;
-  // public amount: number;
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('patrick', 'work on the patrick website', 250);
 const invTwo = new Invoice('robert', 'work on the robert website', 300);
