99bf572493fbb7ac642709adfb4c90243873ef35
Patrick <patrick204nqh@gmail.com>
2020-05-23 13:21:00 +0700

    rendering a HTML template

--- public/app.js
+++ public/app.js
@@ -1,24 +1,15 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('patrick', 'web work', 250);
-// docTwo = new Payment('robert', 'plumbing work', 200);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
-// const invOne = new Invoice('patrick', 'work on the patrick website', 250);
-// const invTwo = new Invoice('robert', 'work on the robert website', 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
+import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -28,5 +19,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
